255fabc01f6a8cd3411b672d7c0d2eabace274f6
mfikriam <yapkun17@gmail.com>
2023-05-29 21:18:33 +0800

    fix formatting

--- src/api/exports/index.js
+++ src/api/exports/index.js
@@ -5,12 +5,7 @@ module.exports = {
   name: 'exports',
   version: '1.0.0',
   register: async (server, { playlistsService, exportsService, validator }) => {
-    const exportsHandler = new ExportsHandler(
-      playlistsService,
-
-      exportsService,
-      validator,
-    );
+    const exportsHandler = new ExportsHandler(playlistsService, exportsService, validator);
     server.route(routes(exportsHandler));
   },
 };
